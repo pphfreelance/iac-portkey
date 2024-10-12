e27c8aaa9fe72980af17335258264823a68b413a
Pyae Phyo Hein(MgOu) <pyaephyohein.info.3326@gmail.com>
2024-10-12 19:22:38 +0700

    add role

--- dev/vpc/state.tf
+++ dev/vpc/state.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3"{
     key = "dev/vpc.tfstate"
     bucket = "${local.backend_name}-state"
-    assume_role {
+    assume_role = {
       role_arn = var.role_arn
     }
   }
