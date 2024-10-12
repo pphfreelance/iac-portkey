dc8936746a84a74abe71fc21578533a6a28a4550
Pyae Phyo Hein(MgOu) <pyaephyohein.info.3326@gmail.com>
2024-10-12 19:39:13 +0700

    add role

--- dev/vpc/state.tf
+++ dev/vpc/state.tf
@@ -2,9 +2,6 @@ terraform {
   backend "s3"{
     key = "dev/vpc.tfstate"
     bucket = "${local.backend_name}-state"
-    assume_role = {
-      role_arn = "arn:aws:iam::926233248796:role/Admin_Role",
-    }
   }
 }
 
